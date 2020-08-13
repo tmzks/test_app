a17698e7d3fd7a47f8fd641b0e1e03582cf2d714
LJW <kwk8711@naver.com>
2020-08-14 05:14:45 +0900

    Updates

---
+++ precache-manifest.0cfe6f4c9e7aacdc16c901ca95da0ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c73eb683c28daf5d3e904479d268c1e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "b428fb409b179baaee2b",
+    "url": "/movie_app_2020/static/css/main.74a557cf.chunk.css"
+  },
+  {
+    "revision": "b669cbd533e71d92c8b4",
+    "url": "/movie_app_2020/static/js/2.27fc80f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.27fc80f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b428fb409b179baaee2b",
+    "url": "/movie_app_2020/static/js/main.f66277b5.chunk.js"
+  },
+  {
+    "revision": "1f22b632cd712202cf6f",
+    "url": "/movie_app_2020/static/js/runtime-main.1b5b4c39.js"
+  }
+]);
